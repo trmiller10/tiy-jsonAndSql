f28df0c7a358f3e5295a2d158510323bcd8202e1
Doug Hughes <doug@theironyard.com>
2016-05-12 11:07:35 -0400

    added list method and some nits

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,21 @@ public class Main {
     public static void main(String[] args){
 
         Spark.get(
+                "/planets",
+                (request, response) -> {
+                    // returns an arraylist of all planets
+                    return null;
+                }
+        );
+
+        Spark.get(
                 "/planet",
                 (request, response) -> {
+                    // queryParam for id?
+                    // or maybe a param like :name and request.params(":name") ?
+
                     Planet planet = new Planet();
-                    planet.name = "Earth";
+                    planet.name = request.params(":name");
                     planet.distanceFromSun = 1;
                     planet.radius = 6387; // km
                     planet.supportsLife = true;
